3af9d5edb571349af2cbfe3fc49483df6e427b5c
Ron <ronniecougar@gmail.com>
2023-10-05 17:30:07 -0400

    working on interface

--- main_tk.py
+++ main_tk.py
@@ -33,16 +33,9 @@ class MyProgram:
     def timestamp(self) -> str:
         return str(dt.now()).replace(':', '.').replace(' ', '-')
 
-    def make_plot(self):
-        self.fig, self.ax = plt.subplots(figsize=(6,6), 
-                                         dpi=100)
-        
-        self.scat = self.ax.scatter(self.data['x'], self.data['y'])
-        return
+    # Don't build any plot objects here, let the interface do that
+    #  # #  ## # # #####  # ## # # # 
     
-    def update_plot(self):
-        self.scat.set_offsets(self.data.values)
-        return
 
     def get_plot(self):
         return self.fig, self.ax
@@ -65,22 +58,67 @@ class MyProgram:
     def make_random_data(self, 
                          num_points=10
                          ):
-        self.data = pd.DataFrame(np.random.normal(size=(num_points,2)), 
+        data = np.random.normal(size=(num_points,2))
+        data[:,1] = data[:,0] + 0.1*data[:,1]
+        self.data = pd.DataFrame(data, 
                                  columns=['x','y'])
         return
     
 class MyInterface:
     # Does all the display stuff
-    def __init__(self):
-        pass
+    def __init__(self,
+                 geometry = '640x480'
+                 ):
+        
+        self.configure_main_window()
+        self.make_plot()
+
+        print('entering main loop')
+        self.main_window.mainloop()
+        print('main loop done')
+        return
+    
+    def configure_main_window(self,
+                              geometry: str = '640x480'
+                              ):
+        self.main_window = tk.Tk()
+        self.main_window.geometry(geometry)
+        self.main_window.title('Example interface using tkinter')
+
+        # Split the main_window into two rows (top for plot, bottom for buttons)
+        self.interface_area = tk.Frame(self.main_window)
+        self.interface_area.rowconfigure(0, weight=1) # The plot area
+        self.interface_area.rowconfigure(1, weight=1) # The buttons area
+
+        
+        # A button
+        print('button stuff')
+        a_button = tk.Button(master=self.interface_area,
+                             command = lambda: print(1),
+                             text = 'a button'
+                             )
+        a_button.grid(row=1, column=0)
+
+        # Pack it up
+        self.interface_area.pack()
+        return
+    
+    def make_plot(self):
+        self.fig, self.ax = plt.subplots(figsize=(2,2), 
+                                         dpi=72)
+        #self.scat = self.ax.scatter(self.data['x'], self.data['y'])
+        self.scat = self.ax.scatter([1,2,3],[4,5,6])
+        self.canvas = FigureCanvasTkAgg(self.fig, 
+                                        master=self.interface_area)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0)
+        return
+    
+    def update_plot(self):
+        self.scat.set_offsets(self.data.values)
         return
     
 if __name__ == '__main__':
     m = MyProgram()
     print(m.timestamp())
-    m.make_random_data()
-    m.make_plot()
-    time.sleep(1)
-    m.make_random_data()
-    
-    
+    MyInterface()

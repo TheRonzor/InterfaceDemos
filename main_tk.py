7b9a32b7a882cba0ab0cba1135f7c36892b6842d
Ron <ronniecougar@gmail.com>
2023-10-05 22:09:40 -0400

    added spiritual plug

--- main_tk.py
+++ main_tk.py
@@ -1,3 +1,11 @@
+##########################################################################
+#                                                                        #
+#      May you do good and not evil                                      #
+#      May you find forgiveness for yourself and forgive others          #
+#      May you share freely, never taking more than you give.            #
+#                                                                        #
+##########################################################################
+
 # Comment out the below (not recommended) to not print The Zen of Python
 print('\n')
 import this
